4c5a6cbdb28f776acbf302e08aa58e7193707b2a
Carla TODORAN-PESCARUS <ctodoran@bitdefender.com>
2023-01-24 23:29:58 +0200

    Functionalities and features

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,22 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'main-manager',
+    loadChildren: () => import('./manager/main-manager/main-manager.module').then(m => m.MainManagerPageModule)
+  },
+  {
+    path: 'main-stats',
+    loadChildren: () => import('./stats/main-stats/main-stats.module').then(m => m.MainStatsPageModule)
+  },
+  {
+    path: 'main-user',
+    loadChildren: () => import('./user/main-user/main-user.module').then(m => m.MainUserPageModule)
+  },
+  {
+    path: 'add-space',
+    loadChildren: () => import('./manager/add-space/add-space.module').then(m => m.AddSpacePageModule)
+  },
 ];
 
 @NgModule({
